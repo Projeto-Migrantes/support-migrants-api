e55a5253213aaf93e4becd7360791075a2d144da
Alisson Oliveira <1272322139@ulife.com.br>
2024-10-30 23:38:12 -0300

    Atualização do cors

--- index.js
+++ index.js
@@ -30,7 +30,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+}))
+
 app.use(limiter);
 
 // Configuração do Swagger
