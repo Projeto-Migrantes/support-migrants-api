646a5a810b342c696244fad0e3680778418a21a5
Alisson Oliveira <1272322139@ulife.com.br>
2024-10-30 23:51:37 -0300

    Atualização do cors

--- index.js
+++ index.js
@@ -30,13 +30,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Configurando o CORS
-app.use(cors({
-    origin: 'http://localhost:8081', 
-    exposedHeaders: ['Authorization'], // Permitir que o cabeçalho Authorization seja acessível
-    allowedHeaders: ['Content-Type', 'Authorization'], // Permitir cabeçalhos que podem ser enviados
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
-}));
-
+app.use(cors());
 
 app.use(limiter);
 
