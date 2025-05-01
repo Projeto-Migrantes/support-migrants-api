f25d344913b3c77872777e718ac40f16f5a6ec21
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-05-01 17:56:43 -0300

    feat(middlewares): update rateLimiter settings

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -1,12 +1,9 @@
 import { rateLimit } from 'express-rate-limit';
 
-/*
-* Rate limiter middleware to limit the number of requests made by a user in a given time frame
-*/
 const limiter = rateLimit({
-    windowMs: 10 * 60 * 1000,
-    max: 100, 
-    message: 'You have made too many requests, please try again later.',
+  windowMs: 10 * 60 * 1000,
+  max: 100,
+  message: 'You have made too many requests, please try again later.',
 });
 
 export default limiter;
