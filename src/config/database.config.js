92c9cd4b3b630f1530f1e592870ff6eb7b3b42d5
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-05-07 14:33:30 -0300

    refactor(config): refactors the database config

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,22 +1,15 @@
 import Sequelize from 'sequelize';
-import dotenv from 'dotenv';
 import pg from 'pg';
-dotenv.config();
 
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT, DB_DIALECT } =
   process.env;
 
-const connection = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    port: process.env.DB_PORT,
-    dialectModule: pg,
-    logging: false,
-  },
-);
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: DB_DIALECT,
+  port: DB_PORT,
+  dialectModule: pg,
+  logging: false,
+});
 
-export default connection;
+export default sequelize;
