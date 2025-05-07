2bef78713302a361b481f51c094a2a093b95a8a3
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-05-07 14:19:52 -0300

    feat(migrations): add services offered migration

---
+++ src/migrations/20250507052338-create-services-offered.js
@@ -0,0 +1,48 @@
+export const up = async (queryInterface, Sequelize) => {
+  await queryInterface.createTable('services_offered', {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    services_offered_pt: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+    },
+    services_offered_fr: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+    },
+    services_offered_es: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+    },
+    services_offered_en: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+    },
+    institutions_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'institutions',
+        key: 'id',
+      },
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.NOW,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.NOW,
+    },
+  });
+};
+
+export const down = async (queryInterface, Sequelize) => {
+  await queryInterface.dropTable('services_offered');
+};
