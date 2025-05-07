b1e63c7931866c5041b90f208e3dde7de5582489
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-05-07 15:02:15 -0300

    feat(config): add logger config

--- src/config/logger.config.js
+++ src/config/logger.config.js
@@ -1,12 +1,6 @@
 import winston from 'winston';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const env = process.env.NODE_ENV || 'development';
-
 const { combine, timestamp, printf, colorize, errors } = winston.format;
 
 const localTimestamp = () => {
@@ -30,39 +24,19 @@ const logger = winston.createLogger({
     errors({ stack: true }),
     logFormat,
   ),
-  transports: [
-    new winston.transports.File({
-      filename: path.join(__dirname, 'logs', 'error.log'),
-      level: 'error',
-    }),
-    new winston.transports.File({
-      filename: path.join(__dirname, 'logs', 'combined.log'),
-    }),
-  ],
+  transports: [],
 });
 
-if (env === 'development') {
-  logger.add(
-    new winston.transports.Console({
-      format: combine(
-        colorize(),
-        timestamp({ format: localTimestamp }),
-        errors({ stack: true }),
-        logFormat,
-      ),
-    }),
-  );
-} else {
-  logger.add(
-    new winston.transports.Console({
-      format: combine(
-        timestamp({ format: localTimestamp }),
-        errors({ stack: true }),
-        logFormat,
-      ),
-    }),
-  );
-}
+logger.add(
+  new winston.transports.Console({
+    format: combine(
+      env === 'development' ? colorize() : winston.format.uncolorize(),
+      timestamp({ format: localTimestamp }),
+      errors({ stack: true }),
+      logFormat,
+    ),
+  }),
+);
 
 export const log = (context = '') => ({
   info: (msg) => logger.info(msg, { context }),
