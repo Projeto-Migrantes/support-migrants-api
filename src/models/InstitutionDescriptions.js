3c2a79f6c44cc4c4f038bb6ec78a1e929e0edb79
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-05-07 14:13:00 -0300

    refactor(model): refactors the institution descriptions model

--- src/models/InstitutionDescriptions.js
+++ src/models/InstitutionDescriptions.js
@@ -1,5 +1,6 @@
 import { DataTypes } from 'sequelize';
 import connection from '../config/database.config.js';
+import Institution from './Institution.js';
 
 const InstitutionDescriptions = connection.define(
   'InstitutionDescriptions',
@@ -11,25 +12,33 @@ const InstitutionDescriptions = connection.define(
       allowNull: false,
     },
     description_pt: {
-      type: DataTypes.STRING(500),
+      type: DataTypes.STRING(255),
       allowNull: false,
     },
     description_fr: {
-      type: DataTypes.STRING(500),
+      type: DataTypes.STRING(255),
       allowNull: false,
     },
     description_es: {
-      type: DataTypes.STRING(500),
+      type: DataTypes.STRING(255),
       allowNull: false,
     },
     description_en: {
-      type: DataTypes.STRING(500),
+      type: DataTypes.STRING(255),
       allowNull: false,
+    },
+    institutions_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: Institution,
+        key: 'id',
+      },
     },
   },
   {
     tableName: 'institutions_descriptions',
-    timestamps: false,
+    timestamps: true,
   },
 );
 
