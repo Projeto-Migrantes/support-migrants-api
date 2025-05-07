696443e17358a560a04fc58f5c21a86c495fd785
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-05-07 14:21:02 -0300

    feat(migrations): add requirements restricitions migration

---
+++ src/migrations/20250507055044-create-requirements-restrictions.js
@@ -0,0 +1,48 @@
+export const up = async (queryInterface, Sequelize) => {
+  await queryInterface.createTable('requirements_restrictions', {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    requirements_restrictions_pt: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+    },
+    requirements_restrictions_fr: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+    },
+    requirements_restrictions_es: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+    },
+    requirements_restrictions_en: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+    },
+    institutions_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'institutions',
+        key: 'id',
+      },
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.NOW,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.NOW,
+    },
+  });
+};
+
+export const down = async (queryInterface, Sequelize) => {
+  await queryInterface.dropTable('requirements_restrictions');
+};
