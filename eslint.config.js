ffecb23f838c9d23f39b8997b54d2bef96e941e6
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-01 21:08:56 -0300

    feat: add eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,21 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs}"],
+    files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
-    extends: ["js/recommended"],
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.es2021,
+      },
+    },
   },
   {
-    files: ["**/*.{js,mjs,cjs}"],
-    languageOptions: { globals: globals.node },
-    rules: {
-      "prettier/prettier": "error",
-    },
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: { globals: globals.browser },
   },
 ]);
