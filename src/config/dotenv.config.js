ca67a580375c1461f00d723030602fb79e814b0e
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-01 21:10:09 -0300

    feat(config): add dotnev config

--- src/config/dotenv.config.js
+++ src/config/dotenv.config.js
@@ -2,4 +2,8 @@ import dotenv from 'dotenv';
 const env = process.env.NODE_ENV || 'development';
 dotenv.config({ path: `.env.${env}.local` });
 
+import checkEnvVariables from '../utils/check-env-variables.js';
+
+checkEnvVariables();
+
 export default dotenv;
