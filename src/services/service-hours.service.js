2d7c2c72b40876ac95ec0220f6b762e2b98a900b
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-02 03:55:21 -0300

    feat(service): update service hours service

--- src/services/service-hours.service.js
+++ src/services/service-hours.service.js
@@ -8,6 +8,10 @@ class ServiceHoursService {
   async create(data, id, transaction) {
     return await serviceHoursRepository.create(data, id, transaction);
   }
+
+  async update(data, id, transaction) {
+    return await serviceHoursRepository.update(data, id, transaction);
+  }
 }
 
 export default new ServiceHoursService();
