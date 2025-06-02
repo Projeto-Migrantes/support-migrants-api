fc7f12e2595b65acac13a0b50fa36c22856bf2dd
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-02 03:53:04 -0300

    feat(repository): add create service hours function

--- src/repositories/service-hours.repository.js
+++ src/repositories/service-hours.repository.js
@@ -4,6 +4,18 @@ class ServiceHoursRepository {
   async findAll() {
     return await ServiceHours.findAll();
   }
+
+  async create(data, id, transaction) {
+    return await ServiceHours.create(
+      {
+        ...data,
+        institution_id: id,
+      },
+      {
+        transaction,
+      },
+    );
+  }
 }
 
 export default new ServiceHoursRepository();
