87c3d1bdcd95b1cece4462e9cc4eac8e46795e04
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-01 21:31:41 -0300

    feat(controller): add findByPostalCode handler

---
+++ src/controllers/address.controller.js
@@ -0,0 +1,22 @@
+import addressService from '../services/address.service.js';
+
+class AddressController {
+  async findByPostalCode(req, res) {
+    try {
+      const address = await addressService.findByPostalCode(
+        req.params.postal_code,
+      );
+      if (address.erro) {
+        return res.status(404).json({ message: 'Invalid Postal Code' });
+      }
+      return res.status(200).json(address);
+    } catch (error) {
+      return res.status(500).json({
+        message: 'server error',
+        error: error.message,
+      });
+    }
+  }
+}
+
+export default new AddressController();
