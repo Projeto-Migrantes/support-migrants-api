e522bfad87a11fc1620a3b2886dd3936603e090e
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-02 03:44:43 -0300

    feat(repository): add create requirement restriction function

--- src/repositories/requirement-restriction.repository.js
+++ src/repositories/requirement-restriction.repository.js
@@ -4,6 +4,16 @@ class RequirementRestrictionRepository {
   async findAll() {
     return await RequirementRestriction.findAll();
   }
+
+  async create(data, id, transaction) {
+    return await RequirementRestriction.create(
+      {
+        ...data,
+        institution_id: id,
+      },
+      { transaction },
+    );
+  }
 }
 
 export default new RequirementRestrictionRepository();
