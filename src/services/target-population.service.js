3ece90668600edb893c2a7e0c40db61ed78ad2af
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-02 04:06:30 -0300

    feat(service): add update target population logic

--- src/services/target-population.service.js
+++ src/services/target-population.service.js
@@ -8,6 +8,10 @@ class TargetPopulationService {
   async create(data, id, transaction) {
     return await targetPopulationRepository.create(data, id, transaction);
   }
+
+  async update(data, id, transaction) {
+    return await targetPopulationRepository.update(data, id, transaction);
+  }
 }
 
 export default new TargetPopulationService();
