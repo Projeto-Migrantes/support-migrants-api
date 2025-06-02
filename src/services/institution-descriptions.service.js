35c8ccc098c7b39e2acf41dd5198f3938029e87f
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-02 03:06:42 -0300

    feat(service): create institution descriptions logic

--- src/services/institution-descriptions.service.js
+++ src/services/institution-descriptions.service.js
@@ -4,6 +4,22 @@ class InstitutionDescriptionsService {
   async findAll() {
     return await institutionDescriptionsRepository.findAll();
   }
+
+  async create(data, id, transaction) {
+    return await institutionDescriptionsRepository.create(
+      data,
+      id,
+      transaction,
+    );
+  }
+
+  async update(data, id, transaction) {
+    return await institutionDescriptionsRepository.update(
+      data,
+      id,
+      transaction,
+    );
+  }
 }
 
 export default new InstitutionDescriptionsService();
