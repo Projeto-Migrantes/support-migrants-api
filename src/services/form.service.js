4817042b082ebd0e751aa64f25a0faf946f0f9ce
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-02 02:54:56 -0300

    feat(service): findById form logic

--- src/services/form.service.js
+++ src/services/form.service.js
@@ -4,6 +4,15 @@ class FormService {
   async findAll() {
     return await formRepository.findAll();
   }
+
+  async findById(id) {
+    const form = await formRepository.findById(id);
+
+    if (!form) {
+      throw new Error('form not found');
+    }
+    return form;
+  }
 }
 
 export default new FormService();
