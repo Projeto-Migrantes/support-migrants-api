276f9ef3b94b1ae19ea43d380230172c3fcc64c8
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-02 03:40:01 -0300

    feat(route): add GET /profile/:id migrant endpoit

--- src/routes/migrant.routes.js
+++ src/routes/migrant.routes.js
@@ -1,28 +1,16 @@
 import { Router } from 'express';
-import migrantController from '../controllers/migrantController.js';
-import validateMigrant from '../middlewares/validation/validateMigrant.js';
-import authenticationMigrant from '../middlewares/auth/authenticationMigrant.js';
+import migrantController from '../controllers/migrant.controller.js';
+import { validateID } from '../middlewares/validation/validateID.js';
+import validateRequest from '../middlewares/validation/validateRequest.js';
+import {
+  createMigrantMainSchema,
+  updateMigrantMainSchema,
+} from '../schemas/migrants/migrant-main.schema.js';
+import { verifyRole } from '../middlewares/auth/role.auth.js';
+import { verifyAdmin } from '../middlewares/auth/admin.auth.js';
 
 const router = Router();
 
-router.post('/check-email', migrantController.emailExist);
-router.get(
-  '/profile',
-  authenticationMigrant.authenticateToken,
-  migrantController.getProfile,
-);
-router.post(
-  '/auth/verify',
-  authenticationMigrant.authenticateToken,
-  migrantController.exist,
-);
-router.get('/count', migrantController.count);
-router.patch('/change-password/:id', migrantController.updatePassword);
-router.get('/search', migrantController.searchMigrants);
-router.get('/', migrantController.findAll);
-router.get('/:id', migrantController.findById);
-router.post('/', validateMigrant, migrantController.create);
-router.put('/:id', migrantController.update);
-router.delete('/:id', migrantController.destroy);
+router.get('/profile/:id', verifyRole, migrantController.profile);
 
 export default router;
