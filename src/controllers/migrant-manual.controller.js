c3f0a0a2800ae90744329dabd32a5b4dbc4bb003
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-02 03:30:19 -0300

    feat(controller): add findAll migrant manual handler

--- src/controllers/migrant-manual.controller.js
+++ src/controllers/migrant-manual.controller.js
@@ -12,6 +12,23 @@ class MigrantManualController {
       });
     }
   }
+
+  async findAll(req, res) {
+    try {
+      let manuals;
+      if (req.query.language) {
+        manuals = await manualService.findByLanguage(req.query.language);
+      } else {
+        manuals = await manualService.findAll();
+      }
+      return res.status(200).json({ data: manuals });
+    } catch (error) {
+      return res.status(500).json({
+        message: 'server error',
+        error: error.message,
+      });
+    }
+  }
 }
 
 export default new MigrantManualController();
