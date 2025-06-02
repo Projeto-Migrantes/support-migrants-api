4e5eac63748fe78392d7cfffd28ef57abf60d8f9
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-01 22:22:59 -0300

    feat: add loginAdmin function

---
+++ src/middlewares/auth/admin.auth.js
@@ -0,0 +1,43 @@
+import jwt from 'jsonwebtoken';
+import { compareHash } from '../../utils/hash-password.util.js';
+import adminService from '../../services/admin.service.js';
+import 'dotenv/config';
+
+export const loginAdmin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const admin = await adminService.findByEmail(email);
+
+    if (!admin) {
+      return res.status(403).json({ message: 'invalid credentials' });
+    }
+
+    const isPasswordValid = await compareHash(password, admin.password);
+
+    if (!isPasswordValid) {
+      return res.status(403).json({ message: 'invalid credentials' });
+    }
+
+    const token = jwt.sign(
+      {
+        sub: admin.id,
+        email: admin.email,
+        full_name: admin.full_name,
+        role: 'admin',
+      },
+      process.env.JWT_SECRET,
+    );
+
+    return res.status(200).json({
+      token,
+    });
+  } catch (error) {
+    if (error.message === 'admin not found') {
+      return res.status(403).json({ message: 'invalid credentials' });
+    }
+    return res.status(500).json({
+      message: 'server error',
+      error: error.message,
+    });
+  }
+};
