0920f9a3775ecab5cda68e65366420f24fde6538
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-02 03:15:40 -0300

    feat(controller): add findById institution handler

--- src/controllers/institution.controller.js
+++ src/controllers/institution.controller.js
@@ -13,6 +13,21 @@ class InstitutionController {
       });
     }
   }
+
+  async findById(req, res) {
+    try {
+      const instituion = await institutionService.findById(req.params.id);
+      return res.status(200).json({ data: instituion });
+    } catch (error) {
+      if (error.message === 'institutions not found') {
+        return res.status(404).json({ error: 'institutions not found' });
+      }
+      return res.status(500).json({
+        message: 'server error',
+        error: error.message,
+      });
+    }
+  }
 }
 
 export default new InstitutionController();
