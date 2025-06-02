cf377f59ed39c9650862511375b7d57ca7825f3b
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-02 04:03:59 -0300

    feat(repository): add create target population function

--- src/repositories/target-population.repository.js
+++ src/repositories/target-population.repository.js
@@ -4,6 +4,16 @@ class TargetPopulationRepository {
   async findAll() {
     return await TargetPopulation.findAll();
   }
+
+  async create(data, id, transaction) {
+    return await TargetPopulation.create(
+      {
+        ...data,
+        institution_id: id,
+      },
+      { transaction },
+    );
+  }
 }
 
 export default new TargetPopulationRepository();
