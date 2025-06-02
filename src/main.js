2e0040063a3dd022ce3d4fa30eece0452521a613
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-01 21:11:16 -0300

    feat: add main file

---
+++ src/main.js
@@ -0,0 +1,32 @@
+import './config/dotenv.config.js';
+import sequelize from './config/database.config.js';
+import logger from './config/logger.config.js';
+import app from './config/express.config.js';
+
+const initializeDatabase = async () => {
+  try {
+    await sequelize.authenticate;
+    logger.info('Database connection established successfully.', {
+      context: 'Database',
+    });
+  } catch (error) {
+    logger.error('Error establishing database connection', {
+      context: 'Database',
+    });
+    process.exit(1);
+  }
+};
+
+const initializeServer = async () => {
+  const port = process.env.SV_PORT ?? 3000;
+  app.listen(port, () => {
+    logger.info(`Server is running on port ${port}...`, { context: 'Server' });
+  });
+};
+
+const bootstrap = async () => {
+  await initializeDatabase();
+  initializeServer();
+};
+
+bootstrap();
