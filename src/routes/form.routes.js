4296477560bf62b97552905d82ef65907184bc7b
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-02 03:03:06 -0300

    feat(route): add GET / form endpoit

--- src/routes/form.routes.js
+++ src/routes/form.routes.js
@@ -1,15 +1,13 @@
 import { Router } from 'express';
-import formController from '../controllers/formController.js';
-import validateForm from '../middlewares/validation/validateForm.js';
+import formController from '../controllers/form.controller.js';
+import { validateID } from '../middlewares/validation/validateID.js';
+import { verifyAdmin } from '../middlewares/auth/admin.auth.js';
+import { verifyMigrant } from '../middlewares/auth/migrant.auth.js';
+import validateRequest from '../middlewares/validation/validateRequest.js';
+import { createFormSchema } from '../schemas/form.schema.js';
 
 const router = Router();
 
-router.get('/', formController.findAll);
-router.get('/count/unread', formController.countUnread);
-router.get('/status/:status', formController.findyByStatus);
-router.put('/:id', formController.update);
-router.get('/:id', formController.findById);
-router.delete('/id', formController.destroy);
-router.post('/', validateForm, formController.create);
+router.get('/', verifyAdmin, formController.findAll);
 
 export default router;
