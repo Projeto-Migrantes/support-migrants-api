814973972ef44e33969e49a4c5bdff3ace4e3213
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-01 21:33:20 -0300

    feat(service): exists logic

--- src/services/address.service.js
+++ src/services/address.service.js
@@ -5,6 +5,17 @@ class AddressService {
   async findByPostalCode(postalCode) {
     return await fetchAddressByPostalCode(postalCode);
   }
+
+  async exists(postalCode, transaction) {
+    let address = await addressRepository.exists(postalCode);
+    if (!address) {
+      address = await this.create(
+        await this.findByPostalCode(postalCode, transaction),
+      );
+    }
+
+    return address;
+  }
 }
 
 export default new AddressService();
