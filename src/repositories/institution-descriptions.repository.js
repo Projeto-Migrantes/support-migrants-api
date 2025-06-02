9ea62c28be6458511455afe8581c3c0c25455ab2
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-02 03:06:21 -0300

    feat(repository): create institution descriptions function

--- src/repositories/institution-descriptions.repository.js
+++ src/repositories/institution-descriptions.repository.js
@@ -4,6 +4,18 @@ class InstitutionDescriptionsRepository {
   async findAll() {
     return await InstitutionDescriptions.findAll();
   }
+
+  async create(data, id, transaction) {
+    return await InstitutionDescriptions.create(
+      {
+        ...data,
+        institution_id: id,
+      },
+      {
+        transaction,
+      },
+    );
+  }
 }
 
 export default new InstitutionDescriptionsRepository();
