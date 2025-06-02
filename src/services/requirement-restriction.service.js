f8b90327a331433c8118134782652cf7249cf09d
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-02 03:46:55 -0300

    feat(service): add create requeriment restriction logic

--- src/services/requirement-restriction.service.js
+++ src/services/requirement-restriction.service.js
@@ -5,6 +5,14 @@ class RequirementRestrictionService {
   async findAll() {
     return await requirementRestrictionRepository.findAll();
   }
+
+  async create(data, instituionId, transaction) {
+    return await requirementRestrictionRepository.create(
+      data,
+      instituionId,
+      transaction,
+    );
+  }
 }
 
 export default new RequirementRestrictionService();
