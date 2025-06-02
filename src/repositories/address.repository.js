115803cf900e1f434ffdb9c8a7ffab5040749fa4
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-01 21:35:08 -0300

    feat(repository): create address function

--- src/repositories/address.repository.js
+++ src/repositories/address.repository.js
@@ -4,6 +4,10 @@ class AddressRepository {
   async exists(postal_code) {
     return await Address.findOne({ where: { postal_code } });
   }
+
+  async create(address) {
+    return await Address.create(address);
+  }
 }
 
 export default new AddressRepository();
