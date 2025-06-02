1167d78138193f993c6d02edb404d74c9e92f15e
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-02 04:03:09 -0300

    feat(repository): add create services offered function

--- src/repositories/services-offered.repository.js
+++ src/repositories/services-offered.repository.js
@@ -4,6 +4,16 @@ class ServicesOfferedRepository {
   async findAll() {
     return await ServicesOffered.findAll();
   }
+
+  async create(data, id, transaction) {
+    return await ServicesOffered.create(
+      {
+        ...data,
+        institution_id: id,
+      },
+      { transaction },
+    );
+  }
 }
 
 export default new ServicesOfferedRepository();
