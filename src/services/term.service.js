b31c58880fa89ff3f0fa52eb8fa79108f4edb5fe
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-02 04:10:13 -0300

    feat(service): add update term logic

--- src/services/term.service.js
+++ src/services/term.service.js
@@ -17,6 +17,14 @@ class TermService {
   async create(data) {
     return await Term.create(data);
   }
+
+  async update(data, type) {
+    const term = await termRepository.update(data, type);
+    if (term === 0) {
+      throw new Error('term not found');
+    }
+    return term;
+  }
 }
 
 export default new TermService();
