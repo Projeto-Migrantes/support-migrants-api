977ad4b4abbd8181f7fbe50b479a5c488dc4ef5d
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-02 04:02:12 -0300

    feat(route): update index route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,25 +2,23 @@ import { Router } from 'express';
 import addressRoutes from './address.routes.js';
 import institutionRoutes from './institution.routes.js';
 import categoryRoutes from './category.routes.js';
-import serviceStationRoutes from './service-station.routes.js';
 import migrantRoutes from './migrant.routes.js';
 import authRoutes from './auth.routes.js';
 import formRoutes from './form.routes.js';
-import pdfRoutes from './pdf.routes.js';
-import userRoutes from './user.routes.js';
+import migrantManualRoutes from './migrant-manual.routes.js';
 import termRoutes from './term.routes.js';
+import adminRoutes from './admin.routes.js';
 
 const router = Router();
 
-router.use('/cep', addressRoutes);
+router.use('/address', addressRoutes);
 router.use('/auth', authRoutes);
 router.use('/categories', categoryRoutes);
 router.use('/forms', formRoutes);
 router.use('/institutions', institutionRoutes);
 router.use('/migrants', migrantRoutes);
-router.use('/pdfs', pdfRoutes);
-router.use('/service-stations', serviceStationRoutes);
+router.use('/migrant-manuals', migrantManualRoutes);
 router.use('/terms', termRoutes);
-router.use('/users', userRoutes);
+router.use('/admins', adminRoutes);
 
 export default router;
