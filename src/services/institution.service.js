ac9a1aca73c3afec73604c5dd34c657abee94ea5
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-02 03:08:18 -0300

    feat(service): findAll institution logic

---
+++ src/services/institution.service.js
@@ -0,0 +1,17 @@
+import institutionDescriptionsService from '../services/institution-descriptions.service.js';
+import institutionRepository from '../repositories/institution.repository.js';
+import addressService from './address.service.js';
+import serviceCostService from './service-cost.service.js';
+import serviceHoursService from './service-hours.service.js';
+import requirementRestrictionService from './requirement-restriction.service.js';
+import servicesOfferedService from './services-offered.service.js';
+import targetPopulationService from './target-population.service.js';
+import sequelize from '../config/database.config.js';
+
+class InstitutionService {
+  async findAll() {
+    return await institutionRepository.findAll();
+  }
+}
+
+export default new InstitutionService();
