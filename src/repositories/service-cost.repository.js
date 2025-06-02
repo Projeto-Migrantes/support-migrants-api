4e4ea24b165729764754de52b7e74440f49fe259
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-02 03:48:09 -0300

    feat(repository): add create service cost function

--- src/repositories/service-cost.repository.js
+++ src/repositories/service-cost.repository.js
@@ -4,6 +4,18 @@ class ServiceCostRepository {
   async findAll() {
     return await ServiceCost.findAll();
   }
+
+  async create(data, id, transaction) {
+    return await ServiceCost.create(
+      {
+        ...data,
+        institution_id: id,
+      },
+      {
+        transaction,
+      },
+    );
+  }
 }
 
 export default new ServiceCostRepository();
