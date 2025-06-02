aba4e7bdc5138f342f73dcf51ed5b94389c574df
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-01 21:37:55 -0300

    feat(repository): create admin function

--- src/repositories/admin.repository.js
+++ src/repositories/admin.repository.js
@@ -1,6 +1,10 @@
 import Admin from '../models/Admin.js';
 
 class AdminRepository {
+  async create(data) {
+    return await Admin.create(data);
+  }
+
   async findByEmail(email) {
     return await Admin.scope('withPassword').findOne({
       raw: true,
