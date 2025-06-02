f39a1d2eb1550b836ea5867c72a45ad7d93b8000
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-02 03:38:04 -0300

    feat(controller): add findById migrant handler

--- src/controllers/migrant.controller.js
+++ src/controllers/migrant.controller.js
@@ -30,6 +30,18 @@ class MigrantController {
       });
     }
   }
+
+  async findById(req, res) {
+    try {
+      const migrant = await migrantService.findById(req.params.id);
+      return res.status(200).json(migrant);
+    } catch (error) {
+      return res.status(500).json({
+        message: 'server error',
+        error: error.message,
+      });
+    }
+  }
 }
 
 export default new MigrantController();
