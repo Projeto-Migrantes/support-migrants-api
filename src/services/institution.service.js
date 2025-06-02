b75d81e5fca40857deef36a5054bf113966ede5f
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-02 03:10:24 -0300

    feat(service): update institution logic

--- src/services/institution.service.js
+++ src/services/institution.service.js
@@ -112,6 +112,84 @@ class InstitutionService {
     }
     return institution;
   }
+
+  async update(data, id) {
+    const t = await sequelize.transaction();
+    try {
+      const institutionExists = await institutionRepository.findById(id);
+      if (!institutionExists) {
+        throw new Error('institution not found');
+      }
+
+      let address = null;
+      if (data.address) {
+        address = await addressService.exists(data.address.postal_code, t);
+        data.institution.address_id = address.id;
+      }
+
+      const institution = await institutionRepository.update(
+        data.institution,
+        id,
+        t,
+      );
+
+      const institution_descriptions =
+        await institutionDescriptionsService.update(
+          data.institution_descriptions,
+          id,
+          t,
+        );
+
+      let requirements_restrictions = null;
+      if (data.requirements_restrictions) {
+        requirements_restrictions = await requirementRestrictionService.update(
+          data.requirements_restrictions,
+          id,
+          t,
+        );
+      }
+
+      const service_cost = await serviceCostService.update(
+        data.service_cost,
+        id,
+        t,
+      );
+
+      const service_hours = await serviceHoursService.update(
+        data.service_hours,
+        id,
+        t,
+      );
+
+      const services_offered = await servicesOfferedService.update(
+        data.services_offered,
+        id,
+        t,
+      );
+
+      const target_populations = await targetPopulationService.update(
+        data.target_populations,
+        id,
+        t,
+      );
+
+      await t.commit();
+
+      return {
+        institution,
+        address,
+        institution_descriptions,
+        requirements_restrictions,
+        service_cost,
+        service_hours,
+        services_offered,
+        target_populations,
+      };
+    } catch (error) {
+      if (!t.finished) await t.rollback();
+      throw error;
+    }
+  }
 }
 
 export default new InstitutionService();
